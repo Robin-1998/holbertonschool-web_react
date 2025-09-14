9441e0ba000f1fc11a16db6348454ce0fa858c3e
Robin-1998 <10616@holbertonstudents.com>
2025-09-14 21:22:44 +0200

    ajout des fichiers tache 6 + 7

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -46,8 +46,34 @@ function createEmployee(salary: number | string): Director | Teacher {
 }
 
 console.log(createEmployee(200));
-Teacher
 console.log(createEmployee(1000));
-Director
-console.log(createEmployee('$500'));
-Director
+
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+// String litteral type
+type Subjects = 'Math' | 'History';
+
+function teachClass (todayClass: Subjects) {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else {
+    return 'Teaching History';
+  }
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
